a3371451ca0b61aad3b003b9be2964c23d60e8f3
Josephwjack <joseph.w.jack@gmail.com>
2022-06-14 15:56:39 -0700

    add event functions, input variables to get elementbyid
    Co-authored-by: Zhibin Liang <ifthereisoneday@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,24 @@ function BankAccount(name, initialDeposit) {
   this.balance = this.balance - withdrawAmount; 
 }
   
-
-
 // UI Logic--------
+let bankAccount = new BankAccount();
+
+
+$(document.ready(function() {
+  $("form#bank").submit(function(event) {
+    event.preventDefault();
+    let inputtedName = $("#inputted-name").val();
+    let inputtedInitialDeposit = $("#inputted-initial-deposit").val();
+    let inputtedDepositAmount = $("#inputted-deposit-amount").val();
+    let inputtedWithdrawAmount = $("#inputted-withdraw-amount").val();
+    if (!Number(inputtedDepositAmount, inputtedInitialDeposit, inputtedWithdrawAmount)) {
+      $(".results").html("Letters are not currency.");
+      $("#balance").toggle();
+    }
+    let newBankAccount = new BankAccount(inputtedName, inputtedInitialDeposit);
+    $("#balanceOutput").html(newBankAccount.balance);
+
+  })
+
+}));
